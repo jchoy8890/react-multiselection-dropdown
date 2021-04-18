67bb2c0d577c232046f262aeba7d8795a8769942
jchoy8890 <jchoy8890@gmail.com>
2021-04-18 00:09:33 -0500

    Add extra data

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,11 +1,11 @@
 
 export const data = [
-    { id: 'car1', label: 'Toyota Yaris', brand: 'Toyota' },
+    { id: 'car1', label: 'Toyota Yaris', brand: 'Toyota', extra: 'some_date' },
     { id: 'car2', label: 'Toyota Corolla', brand: 'Toyota' },
     { id: 'car3', label: 'Toyota Etios', brand: 'Toyota' },
     { id: 'car4', label: 'Toyota Hilux', brand: 'Toyota' },
     { id: 'car5', label: 'Toyota Avanza', brand: 'Toyota' },
-    { id: 'car6', label: 'Nissan Sentra', brand: 'Nissan' },
+    { id: 'car6', label: 'Nissan Sentra', brand: 'Nissan', newdata: 'new_data' },
     { id: 'car7', label: 'Nissan Qashqai', brand: 'Nissan' },
     { id: 'car8', label: 'Nissan Xtrail', brand: 'Nissan' },
     { id: 'car9', label: 'Nissan Patrol', brand: 'Nissan' },
