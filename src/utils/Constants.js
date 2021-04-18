e29836ae154ad66a6f9e9203e3b36e4a9f43731f
jchoy8890 <jchoy8890@gmail.com>
2021-04-18 00:10:40 -0500

    Add image data into select

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,13 +1,13 @@
 
 export const data = [
-    { id: 'car1', label: 'Toyota Yaris', brand: 'Toyota', extra: 'some_date' },
-    { id: 'car2', label: 'Toyota Corolla', brand: 'Toyota' },
-    { id: 'car3', label: 'Toyota Etios', brand: 'Toyota' },
-    { id: 'car4', label: 'Toyota Hilux', brand: 'Toyota' },
-    { id: 'car5', label: 'Toyota Avanza', brand: 'Toyota' },
-    { id: 'car6', label: 'Nissan Sentra', brand: 'Nissan', newdata: 'new_data' },
-    { id: 'car7', label: 'Nissan Qashqai', brand: 'Nissan' },
-    { id: 'car8', label: 'Nissan Xtrail', brand: 'Nissan' },
-    { id: 'car9', label: 'Nissan Patrol', brand: 'Nissan' },
-    { id: 'car10', label: 'Nissan Pathfinder', brand: 'Nissan' },
+    { id: 'car1', label: 'Toyota Yaris', brand: 'Toyota', extra: 'some_date', img: 'image_path' },
+    { id: 'car2', label: 'Toyota Corolla', brand: 'Toyota', img: 'image_path' },
+    { id: 'car3', label: 'Toyota Etios', brand: 'Toyota', img: 'image_path' },
+    { id: 'car4', label: 'Toyota Hilux', brand: 'Toyota', img: 'image_path' },
+    { id: 'car5', label: 'Toyota Avanza', brand: 'Toyota', img: 'image_path' },
+    { id: 'car6', label: 'Nissan Sentra', brand: 'Nissan', newdata: 'new_data', img: 'image_path' },
+    { id: 'car7', label: 'Nissan Qashqai', brand: 'Nissan', img: 'image_path' },
+    { id: 'car8', label: 'Nissan Xtrail', brand: 'Nissan', img: 'image_path' },
+    { id: 'car9', label: 'Nissan Patrol', brand: 'Nissan', img: 'image_path' },
+    { id: 'car10', label: 'Nissan Pathfinder', brand: 'Nissan', img: 'image_path' },
 ];
